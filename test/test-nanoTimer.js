5e73e91ebeafdb937fb380eb25d99232750eaad2
Krb686 <krb686@gmail.com>
2013-01-10 17:22:27 -0500

    fixed timer

--- test/test-nanoTimer.js
+++ test/test-nanoTimer.js
@@ -1,51 +1,50 @@
 var nanoTimer = require('../lib/nanoTimer.js');
-var mocha = require('mocha');
 var should = require('should');
 
 
 
+describe('nanoTimer', function(){
+    it('a high resolution timer object for node', function(){
+        var task = function(){
+            var count = 0;
+            for(var i=0;i<100000;i++){
+                count++;
+            };
+            
+        };
 
-var task = function(){
-	var count = 0;
-	for(var i=0;i<100000;i++){
-		count++;
-	};
-	
-};
+        var task2 = function(){
+            console.log('hey');
+        };
 
-var task2 = function(){
-	console.log('hey');
-};
+        var task3 = function(){
+            nanoTimer.clearInterval();
+        };
 
-var task3 = function(){
-	HrTimer.clearInterval();
-};
+        var task4 = function(){
+            console.log('HELLLOOOO');
+        };
+        
+      //Test 1
+        var numSamples  = 1000;
+        var total   = 0;
+        var avg     = 0;
+        for(var i=0;i<numSamples;i++){
+            var timeTook = nanoTimer.time(task);
+            timeTook = timeTook[0] + (timeTook[1] / 1000000000);
+            total+=timeTook;
+        }
 
-var task4 = function(){
-	console.log('HELLLOOOO');
-};
+        avg = total/numSamples;
+        console.log(avg);
+        console.log('========');
+
+        setInterval(task2, 1000);
+        nanoTimer.setInterval(task2, 1000000000);
+    })
+})
 
 
 
-
-//Begin tests
-
-
-//Test 1
-var numSamples	= 1000;
-var total	= 0;
-var avg		= 0;
-for(var i=0;i<numSamples;i++){
-	var timeTook = HrTimer.time(task);
-	timeTook = timeTook[0] + (timeTook[1] / 1000000000);
-	total+=timeTook;
-}
-
-avg = total/numSamples;
-console.log(avg);
-console.log('========');
-
-setInterval(task2, 1000);
-HrTimer.setInterval(task2, 1000000000);
 
 
